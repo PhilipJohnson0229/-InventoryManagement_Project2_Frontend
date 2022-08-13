5dacd4aa5d745f8d0d3f3e57daf8d72c3a5ab387
Lykasha <lykashalysongtseng@gmail.com>
2022-08-13 17:59:48 -0500

    Imported EditableRow to Items

--- src/components/StoreInventory/EditableRow.js
+++ src/components/StoreInventory/EditableRow.js
@@ -3,11 +3,46 @@ import React from 'react'
 export const EditableRow = () => {
   return (
     <tr>
-        <td>{e.id}</td>
-        <td>{e.name}</td>
-        <td>{e.price}</td>
-        <td>{e.store.name}</td>
-        <td>{e.category.name}</td>
+        <td>
+            <input 
+            type="text" 
+            required="required" 
+            placeholder='Enter an id...' 
+            name="changeId">
+            </input>
+        </td>
+        <td>
+            <input 
+            type="text" 
+            required="required" 
+            placeholder='Enter a item name...' 
+            name="changeName">
+            </input>
+        </td>
+        <td>
+            <input 
+            type="text" 
+            required="required" 
+            placeholder='Enter an price...' 
+            name="changePrice">
+            </input>
+        </td>
+        <td>
+            <input 
+            type="text" 
+            required="required" 
+            placeholder='Enter a store name...' 
+            name="changeStoreName">
+            </input>
+        </td>
+        <td>
+            <input 
+            type="text" 
+            required="required" 
+            placeholder='Enter a category...' 
+            name="changeCategoryName">
+            </input>
+        </td>
     </tr>
   )
 }
